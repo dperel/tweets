9a0d5d864c9c958c08cf16bb427aff631bedc686
David Perel <dmperel@gmail.com>
2017-07-12 15:49:03 -0700

    Remove old test route and add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'test', to: 'test#test'
+  root to: "twitter#main"
 
   get 'main', to: :main, controller: 'twitter'
 
