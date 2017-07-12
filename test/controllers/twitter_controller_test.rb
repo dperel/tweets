054d4a5ae8c0121bbbe42fa9e631c64a64c1fd8f
David Perel <dmperel@gmail.com>
2017-07-12 15:09:36 -0700

    Integration tests

--- test/controllers/twitter_controller_test.rb
+++ test/controllers/twitter_controller_test.rb
@@ -2,10 +2,6 @@ require 'test_helper'
 
 class TwitterControllerTest < ActionDispatch::IntegrationTest
 
-    setup do
-
-    end
-
     test "it can access the main page only when authenticated" do
 
         get '/main'
@@ -15,4 +11,66 @@ class TwitterControllerTest < ActionDispatch::IntegrationTest
         assert_equal 200, status
 
     end
+
+    test "#pull_tweets returns an array of tweets when given a real handle" do
+
+        post '/pull_tweets.json', params: {handle: "nyt"}, headers: {'HTTP_AUTHORIZATION': ActionController::HttpAuthentication::Basic.encode_credentials("twitter", "twitter")}
+
+        assert_equal 200, status
+        json = JSON.parse(response.body)
+        assert_equal Array, json.class
+        refute_empty json
+
+    end
+
+    test "#pull_tweets returns an empty array for a nonexistent handle" do
+
+        post '/pull_tweets.json', params: {handle: "921830912830912830218030912"}, headers: {'HTTP_AUTHORIZATION': ActionController::HttpAuthentication::Basic.encode_credentials("twitter", "twitter")}
+
+        assert_equal 200, status
+        json = JSON.parse(response.body)
+        assert_equal Array, json.class
+        assert_empty json
+
+    end
+
+    test "#pull_tweets linkifies user handles" do
+
+        post '/pull_tweets.json', params: {handle: "walmart"}, headers: {'HTTP_AUTHORIZATION': ActionController::HttpAuthentication::Basic.encode_credentials("twitter", "twitter")}
+
+        assert_equal 200, status
+
+        if response.body.include?("@")
+            assert_includes response.body, "href='http://twitter.com/"
+        else
+            puts "WARNING: This tweet pull from Walmart did not include any handles, which is suspicious."
+        end
+
+    end
+
+    test "Results are cached when done close together" do
+
+        client = Twitter::REST::Client.new do |config|
+			config.consumer_key = ENV["consumer_key"]
+			config.consumer_secret = ENV["consumer_secret"]
+			config.access_token = ENV["access_token"]
+			config.access_token_secret= ENV["access_token_secret"]
+		end
+
+        resp = client.get('/1.1/application/rate_limit_status.json')
+        remaining_before_calls = resp[:resources][:search].values[0][:remaining]
+
+        # call the local endpoint 5 times 
+        5.times do
+            post '/pull_tweets.json', params: {handle: "target"}, headers: {'HTTP_AUTHORIZATION': ActionController::HttpAuthentication::Basic.encode_credentials("twitter", "twitter")}
+        end
+
+        resp_2 = client.get('/1.1/application/rate_limit_status.json')
+        remaining_after_calls = resp_2[:resources][:search].values[0][:remaining]
+
+        # If the requests were not cached, we'd exect the difference to be 5, not 1
+        assert_equal 1, remaining_before_calls - remaining_after_calls
+
+    end
+
 end
